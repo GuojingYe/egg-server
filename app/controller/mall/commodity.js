16b23dedf0e9442eba016eb54a19e16ac364e0dd
GuojingYe <827610124@qq.com>
2018-02-03 17:52:20 +0800

    add ignore

--- app/controller/mall/commodity.js
+++ app/controller/mall/commodity.js
@@ -3,10 +3,6 @@ const Controller = require('egg').Controller;
 class commodityController extends Controller {
     async getCommodity() {
         const {ctx} = this;
-        ctx.logger.info('this is ygj de log');
-        ctx.logger.warn('WARNNING!!!!');
-        ctx.logger.error(new Error('whoops'));
-        ctx.logger.error('error~~@!');
         ctx.doResponse({name: 'ygj'});
     }
 }
